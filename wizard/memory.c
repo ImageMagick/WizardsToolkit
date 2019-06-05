5e0f29fe51fab9a8c7ea0cb69fb279c052d96c1d
Cristy <mikayla-grace@urban-warrior.org>
2019-06-04 21:00:35 -0400

    ...

--- wizard/memory.c
+++ wizard/memory.c
@@ -252,7 +252,7 @@ WizardExport void *AcquireAlignedMemory(const size_t count,const size_t quantum)
     extent=(size+alignment-1)+sizeof(void *);
     if (extent > size)
       {
-        p=malloc(extent);
+        p=AcquireWizardMemory(extent);
         if (p != NULL)
           {
             memory=(void *) AlignedExtent((size_t) p+sizeof(void *),alignment);
@@ -920,7 +920,7 @@ WizardExport void *RelinquishAlignedMemory(void *memory)
 #elif defined(WIZARDSTOOLKIT_HAVE__ALIGNED_MALLOC)
   _aligned_free(memory);
 #else
-  free(*((void **) memory-1));
+  RelinquishWizardMemory(*((void **) memory-1));
 #endif
   return(NULL);
 }
