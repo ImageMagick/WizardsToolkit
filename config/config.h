c869827b7ce86cc6eabfa60c73907f85d0ad47b0
Cristy <mikayla-grace@urban-warrior.org>
2019-10-30 13:30:48 -0400

    ...

--- config/config.h
+++ config/config.h
@@ -68,7 +68,7 @@
 
 /* Define to 1 if you have the declaration of `strlcpy', and to 0 if you
    don't. */
-#define HAVE_DECL_STRLCPY 1
+#define HAVE_DECL_STRLCPY 0
 
 /* Define to 1 if you have the declaration of `vsnprintf', and to 0 if you
    don't. */
@@ -151,7 +151,7 @@
 #define HAVE_LOCALE_H 1
 
 /* Define to 1 if the system has the type `locale_t'. */
-#define HAVE_LOCALE_T 1
+/* #undef HAVE_LOCALE_T */
 
 /* Define to 1 if you have the `localtime_r' function. */
 #define HAVE_LOCALTIME_R 1
@@ -449,7 +449,7 @@
 #define HAVE_VSPRINTF 1
 
 /* Define to 1 if you have the <xlocale.h> header file. */
-#define HAVE_XLOCALE_H 1
+/* #undef HAVE_XLOCALE_H */
 
 /* Define to 1 if you have the `_aligned_malloc' function. */
 /* #undef HAVE__ALIGNED_MALLOC */
--- wizard/wizard-config.h
+++ wizard/wizard-config.h
@@ -101,7 +101,7 @@
 /* Define to 1 if you have the declaration of `strlcpy', and to 0 if you
    don't. */
 #ifndef WIZARDSTOOLKIT_HAVE_DECL_STRLCPY
-#define WIZARDSTOOLKIT_HAVE_DECL_STRLCPY 1
+#define WIZARDSTOOLKIT_HAVE_DECL_STRLCPY 0
 #endif
 
 /* Define to 1 if you have the declaration of `vsnprintf', and to 0 if you
@@ -229,9 +229,7 @@
 #endif
 
 /* Define to 1 if the system has the type `locale_t'. */
-#ifndef WIZARDSTOOLKIT_HAVE_LOCALE_T
-#define WIZARDSTOOLKIT_HAVE_LOCALE_T 1
-#endif
+/* #undef HAVE_LOCALE_T */
 
 /* Define to 1 if you have the `localtime_r' function. */
 #ifndef WIZARDSTOOLKIT_HAVE_LOCALTIME_R
@@ -685,9 +683,7 @@
 #endif
 
 /* Define to 1 if you have the <xlocale.h> header file. */
-#ifndef WIZARDSTOOLKIT_HAVE_XLOCALE_H
-#define WIZARDSTOOLKIT_HAVE_XLOCALE_H 1
-#endif
+/* #undef HAVE_XLOCALE_H */
 
 /* Define to 1 if you have the `_aligned_malloc' function. */
 /* #undef HAVE__ALIGNED_MALLOC */
