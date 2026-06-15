8360ff177b9584846661d266cb2403f2296ad7f9
Cristy <urban-warrior@imagemagick.org>
2026-06-15 07:31:39 -0400

    latest autoconf / automake configuration updates

--- wizard/version.h
+++ wizard/version.h
@@ -33,7 +33,7 @@ extern "C" {
 #define WizardLibAddendum  "-5"
 #define WizardLibInterface  1
 #define WizardLibMinInterface  1
-#define WizardReleaseDate  "2025-12-13"
+#define WizardReleaseDate  "2026-02-15"
 #define WizardChangeDate   "@PACKAGE_CHANGE_DATE@"
 #define WizardAuthoritativeURL  "https://urban-warrior.org/WizardsToolkit"
 #define WizardVersion WizardPackageName " " WizardLibVersionText \
